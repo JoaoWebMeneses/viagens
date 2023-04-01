73176178114b79b620c022bd0c0f8692a9f9baa0
JoaoWebMeneses <joaowebmeneses@gmail.com>
2023-04-01 09:28:15 -0300

    seed configurado

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Travel.create(
+    nome: Faker::Nation.capital_city,
+    desc: Faker::Address.street_address,
+    data: Faker::Date.between(from: '2014-04-01', to: '2023-04-01'),
+    price: Faker::Number.decimal(l_digits: 2),
+)
+
+puts Travel.last
