1125a2db7997a051dbbbd320b7d087d075b4c74b
JoaoWebMeneses <joaowebmeneses@gmail.com>
2023-05-27 09:30:42 -0300

    active storage instalado

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     end
     namespace :v2 do
       resources :travels
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
